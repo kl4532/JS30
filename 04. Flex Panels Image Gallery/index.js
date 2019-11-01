df38fafd282f962cf7ac5b23f50865ca3301e4a6
kl4532 <kornellukasiak@gmail.com>
2019-11-01 09:35:05 +0100

    add text modification

--- 04. Flex Panels Image Gallery/index.js
+++ 04. Flex Panels Image Gallery/index.js
@@ -2,8 +2,17 @@
     const items = document.querySelectorAll('.item');
     items.forEach((item)=>{
         item.addEventListener('click', ()=>{
-            items.forEach((el)=>{ el!=item ? el.classList.remove('active') : 0});
-        item.classList.toggle('active');
+            items.forEach((el)=>{ 
+                if(el != item){
+                    el.classList.remove('active');
+                    el.childNodes[3].innerText = "Click me!";
+                }
+            })
+            let child = item.childNodes[3];
+            if(child.innerText == "Click me!"){
+                child.innerHTML = "&#9786;";
+            }else child.innerText = "Click me!";
+            item.classList.toggle('active');
         });
     })
 })()
