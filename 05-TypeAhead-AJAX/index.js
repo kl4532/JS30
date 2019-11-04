856547ad99333bb823c34bd8b6d55e8be2fd27a8
kl4532 <kornellukasiak@gmail.com>
2019-11-04 10:42:09 +0100

    added insertCommas function

--- 05-TypeAhead-AJAX/index.js
+++ 05-TypeAhead-AJAX/index.js
@@ -3,6 +3,17 @@
     const url = 'https://gist.githubusercontent.com/Miserlou/c5cd8364bf9b2420bb29/raw/2bf258763cdddd704f8ffd3ea9a3e81d25e2c6f6/cities.json';
 
     let allData;
+    fetchData();
+    
+    input.addEventListener('input', () => {
+        document.querySelector('.results').innerHTML = "";
+        filterData(input.value).forEach(el => {
+            let child = document.createElement('li');
+            child.innerHTML = `${el.city}, ${el.state} ${insertCommas(el.pop)}`;
+            document.querySelector('.results').appendChild(child)
+        })
+    })
+
     function fetchData(usrInput){
         fetch(url)
         .then((resp) => resp.json()) // Transform the data into json
@@ -14,16 +25,6 @@
             allData = arr;
         })
     }
-    fetchData();
-    
-    input.addEventListener('input', () => {
-        document.querySelector('.results').innerHTML = "";
-        filterData(input.value).forEach(el => {
-            let child = document.createElement('li');
-            child.innerHTML = `${el.city}, ${el.state} ${el.pop}`;
-            document.querySelector('.results').appendChild(child)
-        })
-    })
     function filterData(input){
         let filtered = [];
         allData.forEach(el => {
@@ -33,6 +34,20 @@
             }
         })
         return filtered;
+    }
+    function insertCommas(testNum){
+        let splitted =  testNum.split("");
+        let result = [];
+        let count = 0;
+        for(let i = testNum.length-1; i>=0; i--){
+            count++;
+            result.unshift(splitted[i]);
+            if(count===3 && i>0){
+                result.unshift(',')
+                count = 0;
+            }
+        }
+        return result.join("");
     }
 })();
 
