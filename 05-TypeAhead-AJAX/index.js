b43c9b896c850b2480d91632a90022f2369a3376
kl4532 <kornellukasiak@gmail.com>
2019-11-04 11:01:28 +0100

    added highlight, start with styling

--- 05-TypeAhead-AJAX/index.js
+++ 05-TypeAhead-AJAX/index.js
@@ -9,7 +9,16 @@
         document.querySelector('.results').innerHTML = "";
         filterData(input.value).forEach(el => {
             let child = document.createElement('li');
-            child.innerHTML = `${el.city}, ${el.state} ${insertCommas(el.pop)}`;
+            const regex = new RegExp('(' + input.value + ')', 'gi');
+            const city= el.city.replace(regex, `<span class="hl">${input.value}</span>`);
+            const state = el.state.replace(regex, `<span class="hl">${input.value}</span>`);
+            let html = `
+            <li>
+                <span class="name">${city}, ${state}</span>
+                <span class="pop">${insertCommas(el.pop)}</span>
+            </li>
+            `
+            child.innerHTML = html
             document.querySelector('.results').appendChild(child)
         })
     })
