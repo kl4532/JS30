bdc87c33b9ae08e1ad3322f8de730b6ca0e12da6
kl4532 <kornellukasiak@gmail.com>
2019-11-10 09:12:49 +0100

    added images, cleanup

--- 06-Canvas-drawing toolbar/index.js
+++ 06-Canvas-drawing toolbar/index.js
@@ -1,66 +1,65 @@
 (function(){
     const c = document.getElementById('myCanvas');
-    c.width = window.innerWidth*0.5;
+    c.width = window.innerWidth*0.7;
     c.height = window.innerHeight*0.9;
     c.style.backgroundColor = "#ffffff";
     const ctx = c.getContext("2d");
-    let mouseDown = false;
+    const root = document.documentElement;
+
+    const inputSize = document.getElementsByName('size')[0];
+    const inputColor = document.getElementsByName('color')[0];
+    const inputFilter = document.getElementsByName('filter')[0];
 
     const images = [];
-    for(let i=0; i<3; i++){
+    for(let i=0; i<5; i++){
         images.push(`./images/${i}.png`);
     }
     let currentImage = 0;
+    let mouseDown = false;
 
-    let inputSize = document.getElementsByName('size')[0];
-    let inputColor = document.getElementsByName('color')[0];
-    let inputBlur = document.getElementsByName('blur')[0];
+    //Init and start
 
-    let size = inputSize.value;
-    let color = inputColor.value;
-    let blur = inputBlur.value;
+    updateProperties();
+    window.onload = addImage();
 
-    inputSize.addEventListener('input', ()=>{size = inputSize.value; mouseDown=false;});
-    inputColor.addEventListener('input', ()=>{color = inputColor.value; mouseDown=false;});
-    inputBlur.addEventListener('input', ()=>{blur = inputBlur.value; mouseDown=false;});
+    //Event Listeners
 
     document.getElementById('clear').addEventListener('click', ()=>{
         confirm("Do you want to clear canvas?") ? ctx.clearRect(0,0,c.width, c.height) : 0;
     });
-
-    const root = document.documentElement;
-    updateProperties();
     root.addEventListener("input", updateProperties);
-    
-    function updateProperties() {
-        root.style.setProperty('--size', document.getElementsByName('size')[0].value + "px");
-        root.style.setProperty('--blur', document.getElementsByName('blur')[0].value + "px");
-        root.style.setProperty('--color', document.getElementsByName('color')[0].value);
-    }
-
-
     c.addEventListener("click", 
         (e)=>{
             mouseDown=true;
             paint(e);
             mouseDown=false;
-        }, 
-    false);
+        }, false);
     c.addEventListener("mousedown", MouseToggle, false);
     c.addEventListener("mousemove", paint, false);
     c.addEventListener("mouseup", MouseToggle, false);
+    document.getElementsByClassName('addImage')[0].addEventListener('click', addImage);
+
+
+    // Functions
+
+    function updateProperties() {
+        root.style.setProperty('--size', inputSize.value + "px");
+        root.style.setProperty('--filter', inputFilter.value + "px");
+        root.style.setProperty('--color', inputColor.value);
+
+        size = inputSize.value; 
+        color = inputColor.value;
+        filter = inputFilter.value; 
+        mouseDown = false;
+    }
+
     function MouseToggle(){
         mouseDown = !mouseDown;
-    }
-    function getPixelColor(x, y) {
-        var pxData = ctx.getImageData(x,y,1,1);
-        return("rgb("+pxData.data[0]+","+pxData.data[1]+","+pxData.data[2]+")");
     }
     function paint(e){
         var rect = e.target.getBoundingClientRect();
         let mouseX = e.pageX - rect.left - document.documentElement.scrollLeft;
         let mouseY = e.pageY -rect.top - document.documentElement.scrollTop;
-        // console.log(`(${mouseX}, ${mouseY})`);
         if(mouseDown){
             
             ctx.beginPath();
@@ -68,28 +67,29 @@
             circle.moveTo(mouseX, mouseY);
             circle.arc(mouseX, mouseY, 0.5*size, 0, 2 * Math.PI);
             ctx.fillStyle = color;
-            ctx.filter = `blur(${blur}px)`;
+            ctx.filter = `blur(${filter}px)`;
             ctx.fill(circle);
             ctx.stroke();
         }
     }
-    document.getElementsByClassName('addImage')[0].addEventListener('click', addImage);
     function addImage(){
-        console.log('test');
         ctx.clearRect(0,0,c.width, c.height);
         ctx.filter = `blur(${0}px)`;
         var image = new Image();    
         image.src = images[currentImage];
         image.onload = function(){
-            console.log(ctx.canvas.width/image.width);
+            // console.log(ctx.canvas.width/image.width);
             ctx.scale(ctx.canvas.width/image.width, ctx.canvas.height/image.height);
-            ctx.drawImage(image,0,0); // Or at whatever offset you like
+            ctx.drawImage(image,0,0);
             ctx.scale(image.width/ctx.canvas.width, image.height/ctx.canvas.height);
         };
         currentImage++;
-        currentImage == images.length-1 ? currentImage = 0 : 0;
+        currentImage == images.length ? currentImage = 0 : 0;
     }
-    window.onload = addImage();
+    function getPixelColor(x, y) {
+        var pxData = ctx.getImageData(x,y,1,1);
+        return("rgb("+pxData.data[0]+","+pxData.data[1]+","+pxData.data[2]+")");
+    }
 })();
 
 
