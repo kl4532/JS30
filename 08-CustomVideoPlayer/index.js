ed8bcc7c83c35de86dab54929d31a66288e68cf4
kl4532 <kornellukasiak@gmail.com>
2019-11-18 17:06:50 +0100

    init

---
+++ 08-CustomVideoPlayer/index.js
@@ -0,0 +1,9 @@
+(function(){
+    const vid = document.getElementById('myVid')
+
+    document.getElementById('btn-play').addEventListener('click', ()=>{
+        vid.paused ? vid.play() : vid.pause();
+    })
+    console.log(vid.mediaKeys);
+
+})()
