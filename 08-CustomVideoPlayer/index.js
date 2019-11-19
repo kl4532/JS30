a95caa3eacbaea410fc8549c817e789513d5dcdd
kl4532 <kornellukasiak@gmail.com>
2019-11-19 18:35:36 +0100

    video pause play

--- 08-CustomVideoPlayer/index.js
+++ 08-CustomVideoPlayer/index.js
@@ -1,10 +1,48 @@
 (function(){
-    const vid = document.getElementById('myVid')
+    const vid = document.getElementById('myVid');
+    const time = document.getElementById('time')
+
+    time.max = vid.duration;
+    
+    let interval;
 
     document.getElementById('btn-play').addEventListener('click', ()=>{
-        vid.paused ? vid.play() : vid.pause();
-    console.log(vid.currentTime);
-
+        time.value = vid.currentTime;
+        vid.paused ? vidPlay() : vidPause();
     })
-    console.log(vid.mediaKeys);
+    
+    time.addEventListener('input', ()=>{
+        vidPause();
+        vid.currentTime = time.value;
+        console.log(vid.currentTime);
+    })
+    time.addEventListener('mouseup', ()=>{
+        vidPlay();
+    })
+
+
+    function videoTime(){
+        let time = vid.currentTime.toFixed(0);
+        return time;
+    }
+    function vidPause(){
+        interval ? clearInterval(interval) : 0;
+        vid.pause();
+        // time.value = vid.currentTime;
+
+    } 
+    function vidPlay(){
+        vid.play();
+        interval = updateVidTime();
+    }
+
+    function updateVidTime(){
+        return setInterval(()=> {
+            time.value = vid.currentTime;
+            console.log(time.value);
+            console.log(vid.paused);
+        }, 1000);
+    }
+    
+
 })()
