9387e9c46d9aff77cdcd9a9b285f315f36d00a3a
kl4532 <kornellukasiak@gmail.com>
2019-11-25 15:47:54 +0100

    more upgrades

--- 08-CustomVideoPlayer/index.js
+++ 08-CustomVideoPlayer/index.js
@@ -2,18 +2,15 @@
     
     const vid = document.getElementById('myVid');
     const time = document.getElementById('time')    
+    const volume = document.getElementById('volume');
     const startPauseBtn = document.getElementById('btn-play');  
-
     
     let interval;
 
-    document.getElementById('btn-play').addEventListener('click', ()=>{
-        !time.max ? time.max = vid.duration: 0;
-        time.value = vid.currentTime;
-        vid.paused ? vidPlay() : vidPause();
-        console.log(time.max);
-    })
-    
+    document.getElementById('btn-play').addEventListener('click', togglePlay);
+
+    vid.addEventListener('click', togglePlay);
+
     time.addEventListener('input', ()=>{
         vidPause();
         vid.currentTime = time.value;
@@ -23,11 +20,25 @@
         !vid.paused ? vidPlay(): vidPause();
         console.log('play');
     })
+    volume.addEventListener('input', ()=>{
+        vid.volume = volume.value; 
+        console.log(vid.volume); 
+    })
 
-
+    function togglePlay() {
+        !time.max ? time.max = vid.duration: 0;
+        time.value = vid.currentTime;
+        vid.paused ? vidPlay() : vidPause();
+        console.log(time.max);
+    }
     function videoTime(){
-        let time = vid.currentTime.toFixed(0);
-        return time;
+        function display(seconds){
+            const format = val => `0${Math.floor(val)}`.slice(-2)
+            const hours = seconds / 3600
+            const minutes = (seconds % 3600) / 60
+            return hours >= 1 ? [hours, minutes, seconds % 60].map(format).join(':') : [minutes, seconds % 60].map(format).join(':');
+        }
+        return display(vid.currentTime) + ' / ' + display(vid.duration);     
     }
     function vidPause(){
         interval ? clearInterval(interval) : console.log('no inte');;
@@ -43,6 +54,7 @@
     function updateVidTime(){
         return setInterval(()=> {
             time.value = vid.currentTime;
+            document.getElementById('vidTime').innerHTML = videoTime();
             vid.currentTime === vid.duration ? clearInterval(interval): 0;
             // console.log(vid.duration, time.value);
         }, 10);
